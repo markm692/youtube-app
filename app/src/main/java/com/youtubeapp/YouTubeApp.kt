459dfbc7bb5ea530acd29840adec6adca020b3b9
markm692 <markm692@users.noreply.github.com>
2026-03-18 01:08:37 +0100

    Restore YouTubeApp with repository for ViewModels, keep minimal MainActivity

--- app/src/main/java/com/youtubeapp/YouTubeApp.kt
+++ app/src/main/java/com/youtubeapp/YouTubeApp.kt
@@ -2,4 +2,12 @@ package com.youtubeapp
 
 import android.app.Application
 
-class YouTubeApp : Application()
+class YouTubeApp : Application() {
+    lateinit var repository: com.youtubeapp.data.repository.YouTubeRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        val apiService = com.youtubeapp.data.remote.RetrofitInstance.apiService
+        repository = com.youtubeapp.data.repository.YouTubeRepository(apiService)
+    }
+}
